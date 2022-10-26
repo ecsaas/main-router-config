8b5267e51806d467af2727423cb1c9c74563ee71
manhbv.giaminh <manhbv.giaminh@gmail.com>
2022-10-26 09:47:18 +0700

    fix prefix.go

--- DEFINE_VARIABLES/ROUTERS/PREFIX/prefix.go
+++ DEFINE_VARIABLES/ROUTERS/PREFIX/prefix.go
@@ -2,6 +2,9 @@ package PREFIX
 
 const API = "/api/"
 const AUTH = "/auth/"
+const ADMIN = "/admin/"
+const FILES = "/files/"
+const OPEN_API = "/open-api/"
 const DOWNLOAD = "/download/"
 const DOCUMENTS = "/documents/"
 const WEB = "/"
